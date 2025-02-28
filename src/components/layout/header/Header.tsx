9bc31be32def6c7b389db1ba3a88735f2031de3e
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2025-02-28 15:03:24 +0600

    features/header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,14 +1,57 @@
-import { FC } from "react";
-import scss from "./Header.module.scss";
+"use client";
+import { FC, useState } from "react";
+import s from "./Header.module.scss";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
+import { IoIosMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const Header: FC = () => {
- return (
-  <section className={scss.Header}>
-   <div className="container">
-    <div className={scss.content}>Header</div>
-   </div>
-  </section>
- );
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <header id={s.header}>
+      <div className="container">
+        <div className={s.header}>
+          <h1>Logo</h1>
+          <span className={s.burger_menu} onClick={() => setOpenModal(true)}>
+            <IoIosMenu />
+          </span>
+          <nav>
+            <Link href="#">Главная</Link>
+            <Link href="#">О нас</Link>
+            <Link href="#">Курсы</Link>
+            <Link href="#">Контакты</Link>
+          </nav>
+          <div className={s.header_btns}>
+            <Link href={"#"}>Войти</Link>
+            <button>
+              Присоединяйся <FaArrowRight />
+            </button>
+          </div>
+        </div>
+        {openModal && (
+          <div className={`${s.header_mobile} ${openModal ? s.active : ""}`}>
+            <span className={s.burger_menu} onClick={() => setOpenModal(false)}>
+              <IoClose />
+            </span>
+            <nav>
+              <Link href="#">Главная</Link>
+              <Link href="#">О нас</Link>
+              <Link href="#">Курсы</Link>
+              <Link href="#">Контакты</Link>
+            </nav>
+            <div className={s.header_btns}>
+              <Link href={"#"}>Войти</Link>
+              <button>
+                Присоединяйся <FaArrowRight />
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
