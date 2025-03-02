88e2d0c6dbd047ab30e8bb93166120d676bce732
맥북 <maegbug@maegbug-ui-MacBookAir.local>
2025-03-02 16:53:01 +0600

    sdas

--- src/components/pages/mainPage/course/Cours.tsx
+++ src/components/pages/mainPage/course/Cours.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { FC } from "react";
 import scss from "./Cours.module.scss";
 import Image from "next/image";
 import cours from "@/assets/img/coursimg.png";
+import Card from "@/ui/card/Card";
+
 const Cours: FC = () => {
   return (
     <section className={scss.Cours}>
@@ -24,12 +27,21 @@ const Cours: FC = () => {
               использовать. Постепенное накопление информация{" "}
             </p>
             <div className={scss.forbutton}>
-                <button>Все курсы</button>
-                <button>Управление компанией</button>
-                <button>Командаобразование</button>
-                <button>Маркетинг</button>
-                <button>Продажи</button>
+              <button>Все курсы</button>
+              <button>Управление компанией</button>
+              <button>Командаобразование</button>
+              <button>Маркетинг</button>
+              <button>Продажи</button>
             </div>
+          </div>
+          <div className={scss.forcard}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <h2>Смотреть больше</h2>
           </div>
         </div>
       </div>
