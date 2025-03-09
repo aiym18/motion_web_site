3ef8e100701681732bed5377b0645097d7485bdb
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2025-03-09 18:22:07 +0600

    dropdown

---
+++ src/ui/dropdown/DropDown.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useState } from "react";
+import s from "./DropDown.module.scss";
+import { IoIosArrowDown } from "react-icons/io";
+import VideoCard from "../videoCard/VideoCard";
+import { Lesson } from "@/types/types";
+
+interface DropDownProps {
+  lesson: Lesson;
+}
+
+const DropDown: FC<DropDownProps> = ({ lesson }) => {
+  const [isOpen, setIsOpen] = useState(false); // Состояние открытия
+
+  return (
+    <div id={s.dropdown}>
+      <div className={`${s.dropdown_title}`} onClick={() => setIsOpen(!isOpen)}>
+        <h2>{lesson.title}</h2>
+        <span className={isOpen ? s.rotate : ""}>
+          <IoIosArrowDown />
+        </span>
+      </div>
+
+      {isOpen && (
+        <div className={s.dropdown_content}>
+          {lesson.videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropDown;
